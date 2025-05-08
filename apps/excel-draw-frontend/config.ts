5e008b587e6454dd8827955b2c4a5566d2185237
Your New Username <piyushchoudhary.068@gmail.com>
2025-05-08 09:29:24 +0530

    fix bugs

--- apps/excel-draw-frontend/config.ts
+++ apps/excel-draw-frontend/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:4004";
-export const WS_URL = "ws://localhost:8090";
+export const BACKEND_URL = "https://cocreate-kext.onrender.com";
+export const WS_URL = "https://cocreate-1-3jsl.onrender.com";
